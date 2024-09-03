9589f4568ab70882fcaecbe8fdc19dff4f50d18d
Francisco Eduardo <franciscoeduardo@MacBook-Air-de-Francisco.local>
2024-09-03 20:13:06 -0300

    Created Prisma Service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Post } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller() // Aqui também pode ser passado um parâmetro, que servirá como um prefixo para todas as rotas dele.
 export class AppController {
-  constructor(private readonly appService: AppService) {} // Inversão de dependência aqui.
+  constructor(
+    private appService: AppService,
+    private prisma: PrismaService,
+  ) {} // Inversão de dependência aqui.
 
   // Quando o usuário fizer a requisição get, o método getHello() será chamado.
   // Como nenhum parâmetro é passado no decorator Get(), fica entendido que o
   // getHello() será a rota raíz. Os parâmetros passados se tornarão o nome da rota.
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello()
+  }
+
+  @Post('/hello')
+  async store() {
+    return await this.prisma.user.findMany()
   }
 }
