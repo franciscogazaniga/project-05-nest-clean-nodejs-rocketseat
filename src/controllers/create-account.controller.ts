f08770fb75bab117e8672da935a29ee9f31d6303
Francisco Eduardo <franciscoeduardo@MacBook-Air-de-Francisco.local>
2024-09-05 20:26:57 -0300

    Implemented zod valitations and http request via vscode

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -4,9 +4,20 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { hash } from 'bcryptjs'
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type createAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -14,7 +25,8 @@ export class CreateAccountController {
 
   @Post()
   @HttpCode(201) // Forces that the success code be 201
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema)) // Used for deal with errors related to Zod validations
+  async handle(@Body() body: createAccountBodySchema) {
     const { name, email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
